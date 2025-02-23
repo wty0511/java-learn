e7287e78edb8e04eaa9876d9cabbf035bcc28262
wty0511 <15652198208@163.com>
2025-02-23 10:33:46 +0800

    新建空白项目

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int g = 0;
+        for (int i = 0; i < 10; i++) {
+
+            g += 1;
+        }
+    }
+}
